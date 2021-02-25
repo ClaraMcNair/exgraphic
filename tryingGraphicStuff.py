faf071ff18e49355aa0eb0623ce5800a274f3465
ClaraMcNair <79084200+ClaraMcNair@users.noreply.github.com>
2021-02-25 14:56:38 +0100

    Initial commit

---
+++ tryingGraphicStuff.py
@@ -0,0 +1,27 @@
+rom graphics import *
+import time
+
+win = GraphWin("cool",500, 500)
+pt = Point(250,250)
+cir = Circle(pt,100)
+
+
+def bePINK():
+  for x in range (255):
+      if x<254:
+        win.setBackground(color_rgb(x,0,255))
+        time.sleep(0.03)
+      else:
+        beBLUE()
+
+
+def beBLUE():
+  for x in range (255,0,-1):
+    if x >1:
+      win.setBackground(color_rgb(x,0,255))
+      time.sleep(0.03)
+    else:
+      bePINK()
+    
+
+beBLUE()
